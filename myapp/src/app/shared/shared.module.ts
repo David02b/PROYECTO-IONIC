479cbf77e4283d2bc5f3d95774dd1fa518b38933
David02b <oscardavidbaron70@hotmail.com>
2024-09-30 19:47:26 -0500

    page

--- myapp/src/app/shared/shared.module.ts
+++ myapp/src/app/shared/shared.module.ts
@@ -1,8 +1,7 @@
 import { Component, ComponentFactoryResolver, NgModule } from '@angular/core';
 import { CommonModule, formatCurrency } from '@angular/common';
-import {HeaderComponent } from './Components/header/Header.Component';
-import {customimputComponent } from './Components/custom-input/custom-input.Component';
-import { CustomImputComponent } from './components/header/custom-imput/custom-imput.component';
+import {HeaderComponent } from './components/header/header.component';
+import {CustomInputComponent } from './components/custom-input/custom-input.component';
 import { LogoComponent } from './components/header/logo/logo.component';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -11,14 +10,17 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
   declarations: [
 
     HeaderComponent,
-    CustomImputComponent,
+    CustomInputComponent,
     LogoComponent,
 
   ],
   exports:[
 
     HeaderComponent,
-    CustomImputComponent,
+    CustomInputComponent,
+    LogoComponent,
+    ReactiveFormsModule
+
 
   ],
   imports: [
