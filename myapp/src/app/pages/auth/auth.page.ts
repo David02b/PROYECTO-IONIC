4866f2d87f80533a48f86939db48c4d0cc293756
David02b <oscardavidbaron70@hotmail.com>
2024-10-06 10:31:42 -0500

    pague

--- myapp/src/app/pages/auth/auth.page.ts
+++ myapp/src/app/pages/auth/auth.page.ts
@@ -37,7 +37,62 @@ export class AuthPage implements OnInit {
 
       this.firebaseSVC.signIn(this.form.value as User).then(res =>{
 
-        console.log(res);
+
+        this.getUserInfo(res.user.uid);
+
+      }).catch(error=> {
+
+        console.log(error);
+
+        this.utilsSvc.presentToast({
+
+          message: error.message,
+          duration:2500,
+          color: 'danger',
+          position: 'middle',
+          icon: 'aler-circle-outline'
+        })
+
+      }).finally(() =>{
+
+        loading.dismiss();
+
+      })
+
+
+
+
+    }
+
+  }
+
+  async getUserInfo(uid: string) {
+
+    
+
+    if (this.form.valid) {
+
+      const loading = await this.utilsSvc.loading();
+      await loading.present();
+
+      let path=`users/${uid}`;
+      
+
+      this.firebaseSVC.getDocument(path).then((user: User) =>{
+
+        this.utilsSvc.saveInLocalStorage('user', user);
+        this.utilsSvc.routerLink('/main/home');
+        this.form.reset();
+
+        this.utilsSvc.presentToast({
+
+          message: 'Bienvenido a tu lugar seguro $(user.name)',
+          duration:1500,
+          color: 'danger',
+          position: 'middle',
+          icon: 'person-circle-outline'
+        })
+
 
       }).catch(error=> {
 
--- myapp/src/app/services/firebase.service.ts
+++ myapp/src/app/services/firebase.service.ts
@@ -3,7 +3,7 @@ import {AngularFireAuth} from '@angular/fire/compat/auth';
 import {getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, updateProfile} from 'firebase/auth';
 import { User } from '../models/user.model';
 import {AngularFirestore} from '@angular/fire/compat/firestore';
-import {getFirestore, setDoc, doc} from '@angular/fire/firestore'
+import {getFirestore, setDoc, doc, getDoc} from '@angular/fire/firestore'
 
 @Injectable({
   providedIn: 'root'
@@ -41,5 +41,11 @@ export class FirebaseService {
     return setDoc(doc(getFirestore(), path), data);
   }
   
+  //================OBTENER DOCUMENTO===========//
+
+  async getDocument(path: string){
+    return (await getDoc(doc(getFirestore(), path))).data();
+  }
+
 }
 
